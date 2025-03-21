2e369a121673283078d29cd8b52561391ef12809
alon <alonmardi@mail.tau.ac.il>
2025-03-21 13:27:02 +0200

    some updates to run on sherlock

--- docs/movie_eval.py
+++ docs/movie_eval.py
@@ -8,7 +8,7 @@ from llava.model.builder import load_pretrained_model
 from llava.mm_utils import get_model_name_from_path, process_images, tokenizer_image_token
 from llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN, IGNORE_INDEX
 from llava.conversation import conv_templates, SeparatorStyle
-
+from docs.clip_eval import fix_wsl_paths, save_cls
 import torch
 import cv2
 import numpy as np
@@ -80,17 +80,17 @@ if 'psychology' in hostname:
     # annot_df_path = "/home/Alon/data/summer/annotations/frame_distances_avg_1s_full.csv"
     video_path = r"/home/new_storage/experiments/NND/500 Days Of Summer.2009.720p.BDRip.x264-VLiS.mp4"
 else:
-    video_path = r"D:\Projects\Annotators\data\500 Days Of Summer.2009.720p.BDRip.x264-VLiS.mp4"
+    video_path = r"D:\Projects\Annotators\data\Sherlock.S01E01.A.Study.in.Pink.mkv"
     # video_path = r"E:\moments\Moments_in_Time_layla\training\working\t2z1X76v4ys_178.mp4"
     video_path = video_path.replace("\\", os.sep)
     drive = video_path.split(os.sep)[0]
     video_path = video_path.replace(drive, f'/mnt/{drive[0].lower()}')
-output_dir = Path(video_path).parent / 'clips'
+output_dir = Path(video_path).parent / 'Sherlock'
 output_dir.mkdir(exist_ok=True, parents=True)
-result_path = output_dir.parent / 'llava_3s_video_results_primitives.csv'
+result_path = output_dir / 'llava_3s_video_results_primitives.csv'
 result_df = pd.DataFrame(columns=['seconds', 'start_frame', 'end_frame', 'question', 'response'])
 fps = 25
-offset = 40 * fps
+offset = 243 * fps
 # offset = 0
 vr = VideoReader(video_path, ctx=cpu(0))
 
@@ -107,10 +107,7 @@ for frame_ind in range(offset, len(vr), fps * 3):
 
     # Prepare conversation input
     conv_template = "qwen_1_5"
-    question = (f"Rate the video based on the following parameters: \n1. Facingness: how much the people facing each other?"
-                f"\n2. Distance: how close are the people from each other?"
-                f"\n3. Cooperation: how much the people are cooperating with each other?"
-                f"\n4. Synced movement: how much the people are moving in sync with each other?")
+    question = (f"Describe where each of people is located in the video, what are they doing, and where are they looking")
     full_prompt = f"{DEFAULT_IMAGE_TOKEN}\n {question}"
     conv = copy.deepcopy(conv_templates[conv_template])
     conv.append_message(conv.roles[0], full_prompt)
@@ -128,10 +125,20 @@ for frame_ind in range(offset, len(vr), fps * 3):
         do_sample=False,
         temperature=0,
         max_new_tokens=4096,
+        output_hidden_states=True,
+        return_dict_in_generate=True,
         modalities=["video"],
     )
-    text_outputs = tokenizer.batch_decode(cont, skip_special_tokens=True)
-    print(text_outputs[0])
+    text_outputs = tokenizer.batch_decode(cont.sequences, skip_special_tokens=True)[0]
+    frame_time = frame_ind // fps
+    # convert to minutes : seconds
+    minutes = frame_time // 60
+    seconds = frame_time % 60
+    print(f"Frame: {frame_time} ({minutes}:{seconds})")
+    print(text_outputs)
+    language_latent = cont.hidden_states[-1][-1].view(-1)
+    save_cls(str(frame_ind), language_latent, output_dir=output_dir / 'llm_language_embeds')
+    save_cls(str(frame_ind), image_embeds[0], output_dir=output_dir / 'vision_only_embeds')
 
     clip_res = {'start_frame': video_frames_dd['start_frame'], 'end_frame': video_frames_dd['end_frame'],
                 'seconds': frame_ind // fps,  'question': question, 'response': text_outputs[0]}
@@ -140,35 +147,3 @@ for frame_ind in range(offset, len(vr), fps * 3):
     # save every 10 responses
     if len(result_df) % 10 == 0:
         result_df.to_csv(result_path, index=False)
-# video_frames = load_movie(video_path, 16)
-#
-#
-# print(video_frames.shape) # (16, 1024, 576, 3)
-# image_tensors = []
-# frames = image_processor.preprocess(video_frames, return_tensors="pt")["pixel_values"].half().cuda()
-# image_tensors.append(frames)
-#
-# # Prepare conversation input
-# conv_template = "qwen_1_5"
-# question = f"{DEFAULT_IMAGE_TOKEN}\n Is there a social interaction between at least two people in the video? what sort of interaction is it?"
-#
-# conv = copy.deepcopy(conv_templates[conv_template])
-# conv.append_message(conv.roles[0], question)
-# conv.append_message(conv.roles[1], None)
-# prompt_question = conv.get_prompt()
-#
-# input_ids = tokenizer_image_token(prompt_question, tokenizer, IMAGE_TOKEN_INDEX, return_tensors="pt").unsqueeze(0).to(device)
-# image_sizes = [frame.size for frame in video_frames]
-#
-# # Generate response
-# cont, image_embeds = model.generate(
-#     input_ids,
-#     images=image_tensors,
-#     image_sizes=image_sizes,
-#     do_sample=False,
-#     temperature=0,
-#     max_new_tokens=4096,
-#     modalities=["video"],
-# )
-# text_outputs = tokenizer.batch_decode(cont, skip_special_tokens=True)
-# print(text_outputs[0])
